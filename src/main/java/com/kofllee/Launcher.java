eef13cc4c28f00732aa2cfd813b60ec8536f1ccc
kofllee <111866132+kofllee@users.noreply.github.com>
2022-10-03 22:55:20 +0600

    create project

---
+++ src/main/java/com/kofllee/Launcher.java
@@ -0,0 +1,9 @@
+package com.kofllee;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Launcher is started with LWJGL " + Version.getVersion());
+    }
+}
